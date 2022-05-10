059384c06c28b0f54b8adc342aa7ebd3920d3ed2
Aymane <aymane.drif.22@gmail.com>
2022-05-10 13:38:08 +0100

    trying to deploy

---
+++ deploy.sh
@@ -0,0 +1,16 @@
+#!/usr/bin/env sh
+set -e
+
+npm run build
+
+cd dist
+
+git init
+
+git add -A
+
+git commit -m "Product deployment"
+
+git push -f git@github.com:jokerdf22/aionchronos.git master:production
+
+cd -
